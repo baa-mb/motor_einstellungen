bad010151e051b9a92f4c2b257800eb42c3fe775
baa <34665120+baa-mb@users.noreply.github.com>
2026-06-21 14:28:48 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
 })
 function logotouch() {
     robotbit.MotorStopAll()
-    basic.showNumber(rad_faktor)
+    basic.showNumber(links_rad_faktor)
     autolauf = 0
     basic.pause(500)
     zzz = zzz_vorgabe
@@ -36,19 +36,19 @@ function logotouch() {
         }
         robotbit.MotorStopAll()
     }
-    basic.showString("E" + rad_faktor)
+    basic.showString("E" + links_rad_faktor)
 }
 input.onButtonPressed(Button.A, function () {
-    rad_faktor += -0.1
+    links_rad_faktor += -0.1
     music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-    basic.showNumber(rad_faktor)
+    basic.showNumber(links_rad_faktor)
 })
 function drehimpuls(v: number) {
     robotbit.MotorRunDual(
         robotbit.Motors.M1A,
         v * richtung,
         robotbit.Motors.M2A,
-        v * richtung * rad_faktor
+        v * richtung * links_rad_faktor
     )
 }
 function init() {
@@ -56,12 +56,12 @@ function init() {
     zzz = zzz_vorgabe
     start_speed = 110
     step_size = 10
-    rad_faktor = 1
+    links_rad_faktor = 1
     limit = 14
     // drehimpuls(start_speed)
     basic.showIcon(IconNames.SmallHeart)
     robotbit.MotorStopAll()
-    // basic.showNumber(rad_faktor)
+    // basic.showNumber(links_rad_faktor)
     basic.showLeds(`
         . . # . .
         . # . # .
@@ -75,11 +75,11 @@ function werte_rechnen(receivedNumber: number) {
     basic.clearScreen()
     if (receivedNumber == 2) {
         zzz = zzz_vorgabe
-        rad_faktor = rad_faktor * 1
+        links_rad_faktor = links_rad_faktor * 1
     }
     if (receivedNumber == -1) {
         zzz_vorgabe = zzz_vorgabe - 1
-        rad_faktor = rad_faktor * 1
+        links_rad_faktor = links_rad_faktor * 1
     }
     zzz += receivedNumber
 }
@@ -89,9 +89,9 @@ input.onButtonPressed(Button.AB, function () {
     init()
 })
 input.onButtonPressed(Button.B, function () {
-    rad_faktor += 0.1
+    links_rad_faktor += 0.1
     music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-    basic.showNumber(rad_faktor)
+    basic.showNumber(links_rad_faktor)
 })
 function dauerschleife(warte: number) {
     basic.showNumber(zzz % 10)
@@ -114,7 +114,7 @@ let step_size = 0
 let start_speed = 0
 let zzz_vorgabe = 0
 let zzz = 0
-let rad_faktor = 0
+let links_rad_faktor = 0
 let autolauf = 0
 let richtung = 0
 let wert = 0
